3d0f2f8089402f98370e0d0b93fe2314c51bd455
Dagfinn Reitan <dagfinn.reitan@novacare.no>
2018-02-21 13:43:24 +0100

    Test config update

--- src/js/searchOptions/searchOption.test.js
+++ src/js/searchOptions/searchOption.test.js
@@ -10,5 +10,5 @@
     autoComplete: true,
     hosts: "test.geonorge.no,kartkatalog.test.geonorge.no,register.test.geonorge.no,objektkatalog.test.geonorge.no,editor.test.geonorge.no,produktark.test.geonorge.no,validering.test.geonorge.no,159.162.102.155,kartkatalog.dev.geonorge.no,register.dev.geonorge.no,objektkatalog.dev.geonorge.no,editor.dev.geonorge.no,produktark.dev.geonorge.no,validering.dev.geonorge.no,159.162.102.155,kartkatalog.dev.geonorge.no,register.dev.geonorge.no,objektkatalog.dev.geonorge.no,editor.dev.geonorge.no,produktark.dev.geonorge.no,validering.dev.geonorge.no,159.162.102.155",
     shoppingCartUrl: 'https://kartkatalog.test.geonorge.no/nedlasting',
-    epiBaseUrl: 'http://www.test.geonorge.no'
+    epiBaseUrl: 'https://www.test.geonorge.no'
 }
