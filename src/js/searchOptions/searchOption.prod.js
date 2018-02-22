aba934330498e18e622edd3b250431a15c2436a9
Dagfinn Reitan <dagfinn.reitan@novacare.no>
2018-02-22 13:43:23 +0100

    search option url update

--- src/js/searchOptions/searchOption.prod.js
+++ src/js/searchOptions/searchOption.prod.js
@@ -9,6 +9,6 @@
     localUrl: false,
     autoComplete: true,
     hosts: "geonorge.no,kartkatalog.geonorge.no,register.geonorge.no,objektkatalog.geonorge.no,editor.geonorge.no,produktark.geonorge.no,validering.geonorge.no,159.162.102.155",
-    shoppingCartUrl: 'https://kartkatalog.geonorge.no/nedlasting',
-    epiBaseUrl: 'https://www.geonorge.no'
+    shoppingCartUrl: '//kartkatalog.geonorge.no/nedlasting',
+    epiBaseUrl: '//www.geonorge.no'
 }
